712a289f84461bc6fd67a9e144a66349d583387c
Jovan Nikolov <nikolovjovan@hotmail.com>
2018-09-05 11:16:33 +0200

    Working drag selection (no visual indicator yet)

---
+++ src/FreeArrangePanel/DragSelectionAdorner.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace FreeArrangePanel
+{
+    internal class DragSelectionAdorner : Adorner
+    {
+        public DragSelectionAdorner(UIElement adornedElement, double borderWidth = 1.0,
+            Color? color = null, Point? start = null, Point? end = null) : base(adornedElement)
+        {
+            BorderWidth = borderWidth;
+            Color = color ?? Color.FromArgb(0x40, 0x00, 0x00, 0xFF);
+            StartPoint = start ?? new Point();
+            EndPoint = end ?? new Point();
+        }
+
+        public double BorderWidth { get; set; }
+        public Color Color { get; set; }
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var dpiFactor = 1 / (PresentationSource.FromVisual(this)?.CompositionTarget?.TransformToDevice.M11 ?? 1.0);
+
+            var rect = new Rect(StartPoint, EndPoint);
+            var drawingBrush = new SolidColorBrush(Color);
+            var drawingPen = new Pen(new SolidColorBrush(
+                    Color.FromArgb(255, Color.R, Color.G, Color.B)), BorderWidth * dpiFactor);
+
+            var halfWidth = drawingPen.Thickness / 2;
+            var guidelines = new GuidelineSet();
+
+            guidelines.GuidelinesX.Add(rect.Left + halfWidth);
+            guidelines.GuidelinesX.Add(rect.Right + halfWidth);
+            guidelines.GuidelinesY.Add(rect.Top + halfWidth);
+            guidelines.GuidelinesY.Add(rect.Bottom + halfWidth);
+
+            drawingContext.PushGuidelineSet(guidelines);
+            drawingContext.DrawRectangle(drawingBrush, drawingPen, rect);
+            drawingContext.Pop();
+        }
+    }
+}
--- src/FreeArrangePanel/FreeArrangePanel.cs
+++ src/FreeArrangePanel/FreeArrangePanel.cs
@@ -1,9 +1,151 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace FreeArrangePanel
 {
     public class FreeArrangePanel : Canvas
     {
+        private readonly DragSelectionAdorner mDragSelectionAdorner;
+        private readonly LinkedList<UIElement> mSelectedElements;
 
+        private bool mDragSelecting;
+        private bool mMouseDown;
+
+        private double mDragThreshold = 5.0;
+        private double mSelectionThreshold = 0.5;
+
+        public FreeArrangePanel()
+        {
+            mDragSelectionAdorner = new DragSelectionAdorner(this);
+            mSelectedElements = new LinkedList<UIElement>();
+            // Mouse hit testing does not work on null Background brush!!!
+            Background = Brushes.Transparent;
+        }
+
+        public double DragThreshold
+        {
+            get => mDragThreshold;
+            set
+            {
+                if (value < 1.0) value = 1.0;
+                mDragThreshold = value;
+            }
+        }
+
+        public double SelectionThreshold
+        {
+            get => mSelectionThreshold;
+            set
+            {
+                if (value < 0.0) value = 0.0;
+                else if (value > 1.0) value = 1.0;
+                mSelectionThreshold = value;
+            }
+        }
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseDown(e);
+
+            if (!ReferenceEquals(e.OriginalSource, this)) return;
+            if (e.ChangedButton != MouseButton.Left) return;
+
+            if (mDragSelecting) StopDragging();
+
+            mMouseDown = true;
+            mDragSelectionAdorner.StartPoint = mDragSelectionAdorner.EndPoint = e.GetPosition(this);
+
+            CaptureMouse();
+
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseUp(e);
+
+            if (!ReferenceEquals(e.OriginalSource, this)) return;
+            if (e.ChangedButton != MouseButton.Left) return;
+
+            if (mDragSelecting)
+            {
+                StopDragging();
+                SelectChildren();
+            }
+
+            if (!mMouseDown) return;
+
+            mMouseDown = false;
+            ReleaseMouseCapture();
+
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            base.OnPreviewMouseMove(e);
+            
+            if (mDragSelecting) Drag(e.GetPosition(this));
+            else if (mMouseDown)
+            {
+                var currentPoint = e.GetPosition(this);
+                var dragDistance = Math.Abs((currentPoint - mDragSelectionAdorner.StartPoint).Length);
+
+                if (dragDistance > mDragThreshold)
+                {
+                    mSelectedElements.Clear();
+                    mDragSelecting = true;
+                    StartDragging(e.GetPosition(this));
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        private void Drag(Point endPoint, bool starting = false)
+        {
+            mDragSelectionAdorner.EndPoint = endPoint;
+            var adornerLayer = AdornerLayer.GetAdornerLayer(this);
+            if (starting) adornerLayer?.Add(mDragSelectionAdorner);
+            adornerLayer?.Update();
+        }
+
+        private void StartDragging(Point endPoint)
+        {
+            Drag(endPoint, true);
+        }
+
+        private void StopDragging()
+        {
+            mDragSelecting = false;
+            var adornerLayer = AdornerLayer.GetAdornerLayer(this);
+            adornerLayer?.Remove(mDragSelectionAdorner);
+        }
+
+        private void SelectChildren()
+        {
+            var dragRect = new Rect(mDragSelectionAdorner.StartPoint, mDragSelectionAdorner.EndPoint);
+            Console.WriteLine("Drag rect: " + dragRect);
+
+            foreach (UIElement child in Children)
+            {
+                var childRect = new Rect(new Point(GetLeft(child), GetTop(child)), child.RenderSize);
+                var intersection = Rect.Intersect(dragRect, childRect);
+                Console.WriteLine("Child: " + child + " Rect: " + childRect);
+                var percentage = intersection.IsEmpty
+                    ? 0.0
+                    : intersection.Width * intersection.Height / (childRect.Width * childRect.Height);
+                Console.WriteLine("Intersection: " + intersection + " Percentage: " + percentage);
+                if (percentage > mSelectionThreshold) mSelectedElements.AddLast(child);
+            }
+
+            Console.WriteLine("Selected elements...");
+            foreach (var selectedElement in mSelectedElements) Console.WriteLine(selectedElement.ToString());
+        }
     }
 }
