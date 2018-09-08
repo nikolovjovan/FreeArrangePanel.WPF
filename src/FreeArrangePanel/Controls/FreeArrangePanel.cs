063749993f21495089edbfe0627ea85d7256da66
Jovan Nikolov <nikolovjovan@hotmail.com>
2018-09-08 18:08:42 +0200

    Working CTRL click and CTRL drag, redesign of element selection and code cleanup

--- src/FreeArrangePanel/Controls/FreeArrangePanel.cs
+++ src/FreeArrangePanel/Controls/FreeArrangePanel.cs
@@ -11,25 +11,26 @@ namespace FreeArrangePanel.Controls
 {
     public class FreeArrangePanel : Canvas
     {
-        private readonly DragSelectionAdorner mDragSelectionAdorner;
-        private readonly LinkedList<UIElement> mSelectedElements;
-
-        private bool mDragSelecting;
-        private bool mMouseDown;
-
-        private double mDragThreshold = 5.0;
-        private double mSelectionThreshold = 0.5;
+        #region Public
 
         public FreeArrangePanel()
         {
             mDragSelectionAdorner = new DragSelectionAdorner(this);
-            mSelectedElements = new LinkedList<UIElement>();
-            // Mouse hit testing does not work on null Background brush!!!
-            // TODO: Try to override default null to transparent...
-            Background = Brushes.Transparent;
+            SelectedElements = new List<UIElement>();
+            Background = Brushes.Transparent; // Mouse hit testing does not work when Background is null!
             Focusable = true; // Allow the panel to get keyboard focus
             Loaded += OnPanelLoaded; // Add drag selection adorner on load
             Unloaded += OnPanelUnloaded; // Remove drag selection adorner on unload
+        }
+
+        public new Brush Background
+        {
+            get => base.Background;
+            set
+            {
+                if (value == null) value = Brushes.Transparent;
+                base.Background = value;
+            }
         }
 
         public double DragThreshold
@@ -53,6 +54,215 @@ namespace FreeArrangePanel.Controls
             }
         }
 
+        public bool ForwardMouseEvents { get; set; }
+
+        public IList<UIElement> SelectedElements { get; }
+
+        #endregion
+
+        #region Protected
+
+        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+        {
+            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+
+            if (visualRemoved != null)
+            {
+                var adornerLayer = AdornerLayer.GetAdornerLayer((Visual) visualRemoved);
+                var adorners = adornerLayer.GetAdorners((UIElement) visualRemoved);
+                if (adorners != null)
+                    foreach (var adorner in adorners)
+                        adornerLayer.Remove(adorner);
+            }
+
+            if (visualAdded != null) ((FrameworkElement) visualAdded).Loaded += OnElementLoaded;
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+
+            mCtrlSelecting = (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control;
+
+            if (!ReferenceEquals(e.Source, this))
+            {
+                var element = e.Source as UIElement;
+                if (mCtrlSelecting) SetSelected(element, !GetSelected(element));
+                if (!ForwardMouseEvents) e.Handled = true;
+                return;
+            }
+
+            mDragSelectionAdorner.StartPoint = mDragSelectionAdorner.EndPoint = e.GetPosition(this);
+
+            if (!mCtrlSelecting)
+            {
+                SelectedElements.Clear();
+                foreach (UIElement child in Children) SetSelected(child, false, false);
+            }
+
+            MouseLeftDown = true;
+
+            Focus(); // Attain keyboard focus
+
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonUp(e);
+
+            if (!ReferenceEquals(e.Source, this))
+            {
+                if (!mCtrlSelecting)
+                {
+                    SelectedElements.Clear();
+                    var element = e.Source as UIElement;
+                    foreach (UIElement child in Children) SetSelected(child, ReferenceEquals(child, element));
+                }
+
+                if (!ForwardMouseEvents) e.Handled = true;
+                return;
+            }
+
+            if (DragSelecting)
+            {
+                DragSelecting = false;
+                foreach (UIElement child in Children) SetSelected(child, GetDragSelected(child));
+            }
+
+            MouseLeftDown = false;
+
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            base.OnPreviewMouseMove(e);
+
+            var cursorPosition = e.GetPosition(this);
+
+            if (DragSelecting)
+            {
+                // Update adorner
+                mDragSelectionAdorner.EndPoint = cursorPosition;
+                var adornerLayer = AdornerLayer.GetAdornerLayer(this);
+                adornerLayer.Update();
+
+                // Select elements
+                mCtrlSelecting = (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control;
+                var dragRect = new Rect(mDragSelectionAdorner.StartPoint, mDragSelectionAdorner.EndPoint);
+                foreach (UIElement child in Children)
+                {
+                    var childRect = new Rect(new Point(GetLeft(child), GetTop(child)), child.RenderSize);
+                    var intersection = Rect.Intersect(dragRect, childRect);
+                    var percentage = intersection.IsEmpty
+                        ? 0.0
+                        : intersection.Width * intersection.Height / (childRect.Width * childRect.Height);
+                    var value = mCtrlSelecting && GetSelected(child);
+                    if (percentage >= SelectionThreshold) SetDragSelected(child, !value);
+                    else SetDragSelected(child, value);
+                }
+            }
+            else if (MouseLeftDown)
+            {
+                var dragDistance = Math.Abs((cursorPosition - mDragSelectionAdorner.StartPoint).Length);
+                if (dragDistance > DragThreshold)
+                {
+                    DragSelecting = true;
+                    mDragSelectionAdorner.EndPoint = cursorPosition;
+                    var adornerLayer = AdornerLayer.GetAdornerLayer(this);
+                    adornerLayer.Update();
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.Key != Key.A) return;
+            if ((Keyboard.Modifiers & ModifierKeys.Control) != ModifierKeys.Control) return;
+            foreach (UIElement child in Children) SetSelected(child, true);
+
+            e.Handled = true;
+        }
+
+        #endregion
+
+        #region Private
+
+        private static readonly DependencyProperty SelectedProperty = DependencyProperty.RegisterAttached(
+            "Selected", typeof(bool), typeof(FreeArrangePanel), new PropertyMetadata(false));
+
+        private static readonly DependencyProperty DragSelectedProperty = DependencyProperty.RegisterAttached(
+            "DragSelected", typeof(bool), typeof(FreeArrangePanel), new PropertyMetadata(false));
+
+        private readonly DragSelectionAdorner mDragSelectionAdorner;
+
+        private bool mMouseLeftDown;
+        private bool mCtrlSelecting;
+        private bool mDragSelecting;
+
+        private double mDragThreshold = 5.0;
+        private double mSelectionThreshold = 0.5;
+
+        private bool MouseLeftDown
+        {
+            get => mMouseLeftDown;
+            set
+            {
+                mMouseLeftDown = value;
+                if (value) CaptureMouse();
+                else ReleaseMouseCapture();
+            }
+        }
+
+        private bool DragSelecting
+        {
+            get => mDragSelecting;
+            set
+            {
+                mDragSelecting = value;
+                mDragSelectionAdorner.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private void SetSelected(UIElement element, bool value, bool modifyList = true)
+        {
+            if (element == null) throw new ArgumentNullException(nameof(element));
+            var oldValue = (bool) element.GetValue(SelectedProperty);
+            if (value == oldValue) return;
+            element.SetValue(SelectedProperty, value);
+            SetDragSelected(element, value);
+            if (!modifyList) return;
+            if (value) SelectedElements.Add(element);
+            else SelectedElements.Remove(element);
+        }
+
+        private static bool GetSelected(UIElement element)
+        {
+            if (element == null) throw new ArgumentNullException(nameof(element));
+            return (bool) element.GetValue(SelectedProperty);
+        }
+
+        private static void SetDragSelected(UIElement element, bool value)
+        {
+            if (element == null) throw new ArgumentNullException(nameof(element));
+            element.SetValue(DragSelectedProperty, value);
+            var adornerLayer = AdornerLayer.GetAdornerLayer(element);
+            var adorners = adornerLayer.GetAdorners(element);
+            if (adorners == null) return;
+            foreach (var adorner in adorners) adorner.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private static bool GetDragSelected(UIElement element)
+        {
+            if (element == null) throw new ArgumentNullException(nameof(element));
+            return (bool) element.GetValue(DragSelectedProperty);
+        }
+
         private void OnPanelLoaded(object sender, RoutedEventArgs e)
         {
             Loaded -= OnPanelLoaded;
@@ -74,172 +284,7 @@ namespace FreeArrangePanel.Controls
             var adornerLayer = AdornerLayer.GetAdornerLayer(element);
             adornerLayer.Add(new ArrangeAdorner(element));
         }
-
-        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
-        {
-            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
-
-            if (visualRemoved != null)
-            {
-                // Remove all adorners from the adorner layer...
-                var adornerLayer = AdornerLayer.GetAdornerLayer((Visual) visualRemoved);
-                var adorners = adornerLayer.GetAdorners((UIElement) visualRemoved);
-                if (adorners != null)
-                    foreach (var adorner in adorners)
-                        adornerLayer.Remove(adorner);
-            }
-
-            if (visualAdded != null) ((FrameworkElement) visualAdded).Loaded += OnElementLoaded;
-        }
-
-        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
-        {
-            base.OnPreviewMouseLeftButtonDown(e);
-
-            if (!ReferenceEquals(e.Source, this))
-            {
-                var element = e.Source as UIElement;
-                var ctrlDown = (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control;
-                // TODO: Don't deselect all if the element is selected to allow moving
-                //       (or use Thumb in ArrangeAdorner for moving)
-                // TODO: Deselect all on PreviewMouseLeftButtonUp (like in Windows Explorer)
-                if (!ctrlDown) DeselectAll();
-                if (ctrlDown && mSelectedElements.Contains(element)) DeselectElement(element, true);
-                else SelectElement(element);
-                // TODO: Add a property to specify whether to forward mouse events or not?!?
-                e.Handled = true;
-                return;
-            }
-
-            // TODO: Add CTRL drag and SHIFT drag like in Windows Explorer...
-
-            if (mDragSelecting) StopDragging();
-
-            DeselectAll();
-
-            mMouseDown = true;
-            mDragSelectionAdorner.StartPoint = mDragSelectionAdorner.EndPoint = e.GetPosition(this);
-
-            CaptureMouse();
-            Focus(); // Attain keyboard focus
-
-            e.Handled = true;
-        }
-
-        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
-        {
-            base.OnPreviewMouseLeftButtonUp(e);
-
-            if (!ReferenceEquals(e.OriginalSource, this)) return;
-
-            if (mDragSelecting)
-            {
-                StopDragging();
-                DragSelectElements();
-            }
-
-            if (!mMouseDown) return;
-
-            mMouseDown = false;
-            ReleaseMouseCapture();
-
-            e.Handled = true;
-        }
-
-        protected override void OnPreviewMouseMove(MouseEventArgs e)
-        {
-            base.OnPreviewMouseMove(e);
-
-            if (mDragSelecting) Drag(e.GetPosition(this));
-            else if (mMouseDown)
-            {
-                var currentPoint = e.GetPosition(this);
-                var dragDistance = Math.Abs((currentPoint - mDragSelectionAdorner.StartPoint).Length);
-
-                if (dragDistance > mDragThreshold)
-                {
-                    DeselectAll();
-                    mDragSelecting = true;
-                    StartDragging(e.GetPosition(this));
-                }
-            }
-
-            e.Handled = true;
-        }
-
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            base.OnKeyDown(e);
-
-            if (e.Key != Key.A) return;
-            if ((Keyboard.Modifiers & ModifierKeys.Control) != ModifierKeys.Control) return;
-            SelectAll();
-            e.Handled = true;
-        }
-
-        private void Drag(Point endPoint, bool starting = false)
-        {
-            mDragSelectionAdorner.EndPoint = endPoint;
-            if (starting) mDragSelectionAdorner.Visibility = Visibility.Visible;
-            var adornerLayer = AdornerLayer.GetAdornerLayer(this);
-            adornerLayer.Update();
-            DragSelectElements();
-        }
-
-        private void StartDragging(Point endPoint)
-        {
-            Drag(endPoint, true);
-        }
-
-        private void StopDragging()
-        {
-            mDragSelecting = false;
-            mDragSelectionAdorner.Visibility = Visibility.Collapsed;
-        }
-
-        private void DragSelectElements()
-        {
-            var dragRect = new Rect(mDragSelectionAdorner.StartPoint, mDragSelectionAdorner.EndPoint);
-            foreach (UIElement child in Children)
-            {
-                var childRect = new Rect(new Point(GetLeft(child), GetTop(child)), child.RenderSize);
-                var intersection = Rect.Intersect(dragRect, childRect);
-                var percentage = intersection.IsEmpty
-                    ? 0.0
-                    : intersection.Width * intersection.Height / (childRect.Width * childRect.Height);
-                if (percentage > mSelectionThreshold) SelectElement(child);
-                else DeselectElement(child, true);
-            }
-        }
-
-        private void SelectAll()
-        {
-            foreach (UIElement child in Children) SelectElement(child);
-        }
-
-        private void DeselectAll()
-        {
-            foreach (UIElement child in Children) DeselectElement(child);
-            mSelectedElements.Clear();
-        }
-
-        private void SelectElement(UIElement child)
-        {
-            if (mSelectedElements.Contains(child)) return;
-            mSelectedElements.AddLast(child);
-            var adornerLayer = AdornerLayer.GetAdornerLayer(child);
-            var adorners = adornerLayer.GetAdorners(child);
-            if (adorners == null) return;
-            foreach (var adorner in adorners) adorner.Visibility = Visibility.Visible;
-        }
-
-        private void DeselectElement(UIElement child, bool remove = false)
-        {
-            if (remove) mSelectedElements.Remove(child);
-            var adornerLayer = AdornerLayer.GetAdornerLayer(child);
-            var adorners = adornerLayer.GetAdorners(child);
-            if (adorners == null) return;
-            foreach (var adorner in adorners) adorner.Visibility = Visibility.Collapsed;
-        }
+        
+        #endregion
     }
 }
